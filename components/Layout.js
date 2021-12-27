5d75108ee98e8f2f0c3a36c84fc77880847368c1
poompichayout <poompichayout.kon@dome.tu.ac.th>
2021-12-27 19:14:08 +0700

    API routes and jsconfig

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,32 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from '../styles/Layout.module.css';
+import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-      </Head>
-      <Header />
-      <div className={styles.container}>{children}</div>
-      <Footer />
-    </div>
-  );
+	const router = useRouter();
+	return (
+		<div>
+			<Head>
+			  <title>{title}</title>
+				<meta name="description" content={description} />
+				<meta name="keywords" content={keywords} />
+			</Head>
+
+			<Header />
+
+			{router.pathname == "/" && <Showcase />}
+
+			<div className={styles.container}>{children}</div>
+			<Footer />
+		</div>
+	);
 }
 
 Layout.defaultProps = {
-  title: "DJ Events | Find the hottest parties",
-  description: "Find ther latest DJ and other musical events",
-  keywords: "music, dj, edm, events",
+	title: "DJ Events | Find the hottest parties",
+	description: "Find ther latest DJ and other musical events",
+	keywords: "music, dj, edm, events",
 };
