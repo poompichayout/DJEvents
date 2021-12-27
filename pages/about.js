48ea924b2192b97d433970cc7509497ecd4e9347
poompichayout <poompichayout.kon@dome.tu.ac.th>
2021-12-27 14:52:32 +0700

    Layout, Header and footer

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const About = () => {
   return (
-    <div>
+    <Layout title='About DJ Events'>
+        
       <h1>About</h1>
       <p>This is an app to find the lastest DJ and other musical events</p>
       <p>Version 1.0.0</p>
-    </div>
+    </Layout>
   );
 };
 
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,10 @@
+import Layout from '../../components/Layout';
+
 export default function EventPage() {
+
     return (
-        <div>
+        <Layout>
             <h1>My Event</h1>
-        </div>
+        </Layout>
     )
 }
